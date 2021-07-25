5c2ab3b2e229b13dacf99f678e210d0608557fa7
Steffi Joel <steffi.jerome@yahoo.com>
2021-07-25 14:47:37 -0400

    adding classnames for the filter form and css must be styled accordingly

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -3,18 +3,18 @@ import React from 'react';
 
 const FilterForm=(props)=>{
     return(
-    <form>
-        <div>
-            <p>
-                <button onClick={props.handleAllSubmit}>
+    <form className="my-6">
+        <div className="is-grouped">
+            <p className="control">
+                <button onClick={props.handleAllSubmit} id="allbtn" className="button is-fullwidth" type="submit">
                     I want Questions from All Categories 
                     </button>
             </p>
-            <h2>OR ..</h2>
-            <div>
-                <div>
-                    <div>
-                        <select onChange={props.handleChange}>
+            <h2 className="my-4 control">OR ..</h2>
+            <div className="filed is-grouped">
+                <div className="control is-expanded">
+                    <div className="select is-fullwidth">
+                        <select onChange={props.handleChange} id="select">
                           {props.allCategories.map(category=>(
                         <option id="options" key={category.id} value={category.id}>
                             {category.name}
@@ -24,8 +24,8 @@ const FilterForm=(props)=>{
                     </div>
                 </div>
             </div>
-            <p>
-                <button onClick={props.handleCategorySubmit}>
+            <p className="control">
+                <button onClick={props.handleCategorySubmit} id="searchbtn" className="button is-info" type="submit">
                     Choose Category
                 </button>
             </p>
